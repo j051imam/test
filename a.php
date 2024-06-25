e9f3c5652c54ddf132094fdef6bf61bec5f06438
j051imam <j520051b@mails.cc.ehime-u.ac.jp>
2024-06-26 02:16:43 +0900

    Update a.php

--- a.php
+++ a.php
@@ -1,6 +1,13 @@
 #!/usr/bin/php
 <?php 
+  // ファイルの各行を読み込んで、配列として返す
   foreach(file('/usr/share/dict/words') as $line){
-    // strpos() 関数を使って、文字列 "xy" が含まれる単語を echo "$line"; で表示しなさい。
+    // 改行を削除
+    $line = trim($line);
+    // "xy" が含まれているかどうかを調べる
+    if (strpos($line, 'xy') !== false) {
+      // 含まれていたら、その行を出力
+      echo "$line\n";
+    }
   } 
 ?>
